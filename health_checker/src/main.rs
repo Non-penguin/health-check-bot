f35e1afd3d91f48fb80d3fa907bc51607e509c17
Non-penguin <iine11razer@gmai.com>
2026-02-12 20:43:13 +0900

    完成版

--- health_checker/src/main.rs
+++ health_checker/src/main.rs
@@ -1,36 +1,62 @@
-use reqwest::Error;
+use futures::future::join_all;
+use reqwest::{Client, Error};
+use serde::Deserialize;
+use std::fs;
 use tokio;
 
-async fn check_health(url: &str) -> Result<(), Error> {
-    let client = reqwest::Client::builder()
-        .timeout(std::time::Duration::from_secs(5))
-        .build()?;
+#[derive(Deserialize)]
+struct Config {
+    urls: Vec<String>,
+    timeout: u64,
+}
 
-    let responce = client.get(url).send().await?;
+async fn check_health(client: &Client, url: &String) -> (String, Result<(), Error>) {
+    let response = client.get(url).send().await;
 
-    if responce.status().is_success() {
-        println!("{} is up and running!", url);
-    } else {
-        println!("Failed to reach {}: {}", url, responce.status());
+    match response {
+        Ok(response) => {
+            if response.status().is_success() {
+                println!("{} is up and running!", url);
+                (url.clone(), Ok(()))
+            } else {
+                println!("Failed to reach {}: {}", url, response.status());
+                (url.clone(), Ok(()))
+            }
+        }
+        Err(e) => (url.clone(), Err(e)),
     }
-
-    Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    let urls = vec![
-        "http://192.168.50.60:3000",
-        "http://192.168.50.53:9090",
-        "http://192.168.50.53:3000",
-    ];
+async fn main() {
+    let config_content = match fs::read_to_string("config.toml") {
+        Ok(content) => content,
+        Err(e) => {
+            eprintln!("Error reading config.toml: {}", e);
+            return;
+        }
+    };
 
-    for url in urls {
-        match check_health(url).await {
-            Ok(_) => (),
-            Err(e) => println!("Failed to send request to {} : {}", url, e),
+    let config: Config = match toml::from_str(&config_content) {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("Error parsing config.toml: {}", e);
+            return;
+        }
+    };
+
+    let client = Client::builder()
+        .timeout(std::time::Duration::from_secs(config.timeout))
+        .build()
+        .unwrap();
+
+    let checks = config.urls.iter().map(|url| check_health(&client, url));
+
+    let results = join_all(checks).await;
+
+    for (url, result) in results {
+        if let Err(e) = result {
+            println!("Failed to send request to {}: {}", url, e);
         }
     }
-    
-    Ok(())
 }
